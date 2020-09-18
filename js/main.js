35e41091f4fde93297ac9a0a7ad5fdac3d16f2ca
marizaurus <masha.torbina5@gmail.com>
2020-09-18 11:09:04 +0500

    fix links & cart

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
+let btnCart = document.querySelector("#cart-button");
+let modal = document.querySelector(".modal");
 let close = document.querySelectorAll("#close");
-console.log(close);
 
-cartButton.addEventListener('click', toggleModal);
+let btnCounter = document.querySelectorAll(".button-counter");
+
+btnCart.addEventListener('click', toggleModal);
 close.forEach(item => item.addEventListener('click', toggleModal));
 
 function toggleModal () {
@@ -11,3 +12,15 @@ function toggleModal () {
 }
 
 new WOW().init();
+
+btnCounter.forEach((btn) => {
+    btn.addEventListener('click', (evt) => {
+        let num = +evt.target.parentNode.children[1].textContent;
+        if (evt.target.classList.contains('button-less') && num > 1) {
+            evt.target.parentNode.children[1].textContent = num - 1;
+        } 
+        else if (evt.target.classList.contains('button-more')) {
+            evt.target.parentNode.children[1].textContent = num + 1;
+        }
+    })
+})
